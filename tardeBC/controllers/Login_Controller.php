8a78e32f18e7011c8dac19dc8333553dc3050c17
TexasXThomas <liantorrio@gmail.com>
2022-08-18 20:44:12 -0300

    Realize deber pedido en clase el cual pedia realizar una consulta al servidor sin reiniciar/recargar la pagina, Investigue lo mejor que pude habian muchas formas de hacerlo pero solo una me convencia, cree 2 nuevos archivos, recargas.php y script.js PSDT: Aclaro que tuve varios problemas para acceder a los productos pero los solucione, parece que mi rama le faltaban cosas.

---
+++ tardeBC/controllers/Login_Controller.php
@@ -0,0 +1,53 @@
+<?php
+require_once 'vendor/autoload.php';
+require_once 'auth/Auth.php';
+
+class Login_Controller extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->view->mensaje        = "";
+        $this->view->resultadoLogin = "";
+    }
+
+    //base+login
+    public function render()
+    {
+        //$alumnos = $this->model->get();
+        $this->view->alumnos = "cargado";
+        $this->view->render('login/index');
+    }
+
+    public function ingresar()
+    {
+        $nombre     = $_POST['nombre'];
+        $password   = $_POST['password'];
+        $exitoLogin = $this->model->ingresar($nombre, $password);
+    }
+    public function salir()
+    {
+        //$_SESSION["estalogueado"] = false;
+        unset($_SESSION["estalogueado"]);
+        unset($_SESSION["nombre"]);
+        session_destroy();
+        $this->view->render('index/index');
+
+    }
+    public function login()
+    {
+        $this->view->render('login/login');
+
+    }
+    public function signin(){
+        $user = new Usuario();
+        $user->email = $_POST['email'];
+        $user->password = $_POST['password'];
+        $id = $this->model->signin($user);
+        $_SESSION['id'] = serialize($id);
+        
+
+        
+    }
+    
+        }
